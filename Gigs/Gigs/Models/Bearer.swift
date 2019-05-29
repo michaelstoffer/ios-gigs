259a4fae8bc8e9638a3666e951f4452bc9b175e0
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-05-28 22:39:27 -0400

    Finished Part 2 - Models

---
+++ Gigs/Gigs/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Michael Stoffer on 5/28/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    var token: String
+}
---
+++ Gigs/Gigs/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Michael Stoffer on 5/28/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var username: String
+    var password: String
+}
